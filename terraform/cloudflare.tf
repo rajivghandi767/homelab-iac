1f09a3b3c2327f4c841a58b6f0c5fecae14d3a0c
Rajiv Wallace <rajivghandi972@gmail.com>
2026-06-15 11:33:59 -0400

    refactor: implement CNAME dynamic IP anchor and separate VPN

--- terraform/cloudflare.tf
+++ terraform/cloudflare.tf
@@ -2,16 +2,29 @@ data "cloudflare_zone" "main" {
   name = "rajivwallace.com"
 }
 
-# 1. The Anchor: Root A Record
+# 1. DDNS IP Tracker (Updated by UniFi DDNS)
+resource "cloudflare_record" "ddns_ip" {
+  zone_id = data.cloudflare_zone.main.id
+  name    = "ddns"
+  content = "192.0.2.1" # Dummy IP; managed externally
+  type    = "A"
+  proxied = false
+
+  lifecycle {
+    ignore_changes = [content]
+  }
+}
+
+# 2. The Anchor: Root Record
 resource "cloudflare_record" "root" {
   zone_id = data.cloudflare_zone.main.id
   name    = "@"
-  content = data.google_secret_manager_secret_version.public_ip.secret_data
-  type    = "A"
+  content = cloudflare_record.ddns_ip.hostname
+  type    = "CNAME"
   proxied = true
 }
 
-# 2. Public Services (The web infrastructure you control)
+# 3. Public Services (The web infrastructure you control)
 resource "cloudflare_record" "public_services" {
   for_each = toset([
     "www",
@@ -23,15 +36,23 @@ resource "cloudflare_record" "public_services" {
     "svt", 
     "svt-api",
     "jenkins", 
-    "jellyfin", 
-    "vpn"
+    "jellyfin"
   ])
   
   zone_id = data.cloudflare_zone.main.id
   name    = each.key
-  content = data.google_secret_manager_secret_version.public_ip.secret_data
-  type    = "A"
+  content = cloudflare_record.ddns_ip.hostname
+  type    = "CNAME"
   proxied = true
+}
+
+# 4. Unproxied Services (VPN)
+resource "cloudflare_record" "unproxied_public_services" {
+  zone_id = data.cloudflare_zone.main.id
+  name    = "vpn"
+  content = cloudflare_record.ddns_ip.hostname
+  type    = "CNAME"
+  proxied = false
 }
 
 # 3. Local Services (Your internal LAN infrastructure)
